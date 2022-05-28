e67acfd51dca9bd49440b61d7f03384a2291b007
deehood <dee.hood@gmail.com>
2022-05-28 13:23:49 +0100

    5.14: Blog list tests, step2

--- frontend-part5/src/components/Blog.test.js
+++ frontend-part5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("blog tests", () => {
@@ -26,14 +27,32 @@ describe("blog tests", () => {
         handleRemoveBlog={handleRemoveBlog}
       />
     );
-
-    let element = screen.queryByText("example - someone");
+    let element = screen.queryByText(/example - someone/);
     expect(element).toBeDefined();
 
-    element = screen.queryByText("www.example.com");
+    element = screen.queryByText(/www.example.com/);
     expect(element).toBeNull();
 
-    element = screen.queryByText("likes");
+    element = screen.queryByText(/likes/);
     expect(element).toBeNull();
+  });
+
+  test("renders likes and url after clicking view button", async () => {
+    render(
+      <Blog
+        blog={blog}
+        username={username}
+        token={token}
+        handleLikes={handleLikes}
+        handleRemoveBlog={handleRemoveBlog}
+      />
+    );
+    const user = userEvent.setup();
+    const button = screen.getByText(/view/);
+    await user.click(button);
+    let element = screen.queryByText(/www.example.com/);
+    expect(element).not.toBeNull();
+    element = screen.queryByText(/likes 0/);
+    expect(element).not.toBeNull();
   });
 });
