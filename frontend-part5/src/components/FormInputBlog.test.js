e068285bb962d2c16fb256392dae9975a08d6b74
deehood <dee.hood@gmail.com>
2022-05-29 09:24:29 +0100

    5.16: Blog list tests, step4

--- frontend-part5/src/components/FormInputBlog.test.js
+++ frontend-part5/src/components/FormInputBlog.test.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import FormInputBlog from "./FormInputBlog";
 
 describe("Input blog tests", () => {
-  const newBlog = {
-    title: "New Book",
-    author: "John Wayne",
-    url: "wwww.somethng.org",
-  };
-
   const mockSetNewPost = jest.fn();
   const mockHandleCreateBlog = jest.fn();
 
-  test("form calls event handler with the right detail", async () => {
+  test("form calls event handler with the right details", async () => {
     render(
       <FormInputBlog
         setNewPost={mockSetNewPost}
@@ -24,12 +18,18 @@ describe("Input blog tests", () => {
     const user = userEvent.setup();
 
     const createButton = screen.getByRole("button", { name: /create/ });
-    const input = screen.getAllByRole("textbox")[0];
+    const input = screen.getAllByRole("textbox");
 
-    await user.type(input, "teste");
+    await user.type(input[0], "New Book");
+    await user.type(input[1], "John Wayne");
+    await user.type(input[2], "wwww.something.org");
 
-    await user.click(createButton);
-    expect(mockHandleCreateBlog.mock.calls).toHaveLength(1);
-    expect(mockHandleCreateBlog.mock.calls[0][0].content).toBe("teste");
+    fireEvent.submit(createButton);
+
+    expect(mockHandleCreateBlog.mock.calls[0][1].title).toBe("New Book");
+    expect(mockHandleCreateBlog.mock.calls[0][1].author).toBe("John Wayne");
+    expect(mockHandleCreateBlog.mock.calls[0][1].url).toBe(
+      "wwww.something.org"
+    );
   });
 });
